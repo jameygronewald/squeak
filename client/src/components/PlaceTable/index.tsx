b96893d5876decd3c64a088567d77d922c286e0f
Jamey Gronewald <jrgronewald@gmail.com>
2020-11-25 19:17:54 -0800

    condensed helpers for jwt into one oobject and updated methods in controllers; installed material UI to start styling frontend components

---
+++ client/src/components/PlaceTable/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+interface PlaceTableProps {
+    
+}
+
+const PlaceTable = (props: PlaceTableProps) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PlaceTable;

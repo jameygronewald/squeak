6d81acb3e11e9b23374a62a26ec95b57e5caf842
Jamey Gronewald <jrgronewald@gmail.com>
2020-11-29 18:34:56 -0800

    added a spinner animation to signal loading when fetching places from 3rd party API

--- client/src/pages/SearchResults/index.tsx
+++ client/src/pages/SearchResults/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { savePlace } from '../../redux/places/actions';
+import Spinner from '../../components/Spinner';
+import { clearPlaceState, savePlace } from '../../redux/places/actions';
 import { SearchData } from '../../interfaces';
 import { useSelector } from 'react-redux';
 
@@ -10,21 +11,25 @@ const Search: React.FC = (): JSX.Element => {
   return (
     <>
       <Link to='/dashboard'>
-        <button>Back to dashboard</button>
+        <button onClick={clearPlaceState}>Back to dashboard</button>
       </Link>
-      {fetchedPlaces.map((place: SearchData) => (
-        <ul key={place.yelp_id}>
-          <h3>{place.name}</h3>
-          <li>
-            Address: {place.address1} / {place.city}, {place.state}
-          </li>
-          <li>Phone: {place.phone}</li>
-          <li>Rating: {place.rating}</li>
-          <button type='button' onClick={savePlace(place)}>
-            Save place
-          </button>
-        </ul>
-      ))}
+      {fetchedPlaces.length === 0 ? (
+        <Spinner />
+      ) : (
+        fetchedPlaces.map((place: SearchData) => (
+          <ul key={place.yelp_id}>
+            <h3>{place.name}</h3>
+            <li>
+              Address: {place.address1} / {place.city}, {place.state}
+            </li>
+            <li>Phone: {place.phone}</li>
+            <li>Rating: {place.rating}</li>
+            <button type='button' onClick={savePlace(place)}>
+              Save place
+            </button>
+          </ul>
+        ))
+      )}
     </>
   );
 };

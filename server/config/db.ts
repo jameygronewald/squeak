eb9ce5d320cc7f3b1e15c9762ac0c29eae617696
Jamey Gronewald <jrgronewald@gmail.com>
2021-01-23 17:50:43 -0800

    tweaking db connection

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,13 +1,26 @@
-import pg from 'pg';
+import pg, { Client } from 'pg';
 import { config } from 'dotenv';
 config();
 
-const postgres = new pg.Pool({
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: 5432,
-  database: process.env.DB_DATABASE,
-});
+let postgres: any;
+
+if (process.env.DATABASE_URL) {
+  postgres = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  postgres = new pg.Pool({
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: 5432,
+    database: process.env.DB_DATABASE,
+  });
+}
+
+postgres.connect();
 
 export default postgres;

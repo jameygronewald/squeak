b05abd0dcc7ccf8ca370e3b24d497c530e1dfe1c
Jamey Gronewald <jrgronewald@gmail.com>
2021-01-24 08:25:07 -0800

    simplified helpedrfor doubling apostrophes fto make pg query using regex and single loop

--- client/src/redux/places/helpers.ts
+++ client/src/redux/places/helpers.ts
@@ -1,8 +1,9 @@
 export const addExtraQuote = (...args: string[]) => {
-  const argsWithExtraQuote: string[] = args.map(arg => {
-    const array: string[] = arg.split(`'`);
-    for (let i = 0; i < array.length - 1; i++) array[i] += `''`;
-    return array.join('');
-  });
+  const argsWithExtraQuote: string[] = [];
+
+  args.forEach((arg: string) => {
+    argsWithExtraQuote.push(arg.replace(/'/g, `''`));
+  })
+
   return argsWithExtraQuote;
 };
